0cf0c035edde7d95a5860a732dc1feb9aae61225
clby <1413369390@qq.com>
2017-10-29 12:39:36 +0800

    clby_ufop_NPOI
    clby_ufop_NPOI

---
+++ clby-ufop-NPOI/build.cmd
@@ -0,0 +1,2 @@
+dotnet publish clby-ufop-NPOI/clby-ufop-NPOI.csproj -o obj/Docker/publish
+docker-compose build
---
+++ clby-ufop-NPOI/clby-ufop-NPOI/Controllers/HandlerController.cs
@@ -0,0 +1,47 @@
+﻿using clby_ufop.Core;
+using clby_ufop.Core.Func;
+using clby_ufop.Core.Misc;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace clby_ufop_NPOI.Controllers
+{
+    [Route("Handler")]
+    public class HandlerController : Controller
+    {
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var args = this.GetArgs();
+            var fileContents = await this.GetFileContentsAsync();
+
+
+            var func = AutofacExtensions.ResolveNamed<IFunc>($"Func_{args.Func}");
+            if (func == null)
+            {
+                return this.StatusCode(521);
+            }
+
+            return func.Do(fileContents, args);
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post()
+        {
+            var args = this.GetArgs();
+            var fileContents = await this.GetFileContentsAsync();
+
+
+            var func = AutofacExtensions.ResolveNamed<IFunc>($"Func_{args.Func}");
+            if (func == null)
+            {
+                return this.StatusCode(521);
+            }
+
+            return func.Do(fileContents, args);
+
+        }
+    }
+}
